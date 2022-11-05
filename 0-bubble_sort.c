642f45a1ef541c21993f8750e1f44ce52bb888ca
crysart <idukrystal@gmail.com>
2022-11-05 03:37:07 +0100

    00

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,9 +19,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				for (k = 0; k < size - 1; k++)
-					printf("%i, ", array[k]);
-				printf("%i\n", array[k]);
+				print_array(array);
 				swaps++;
 			}
 		}
--- sort.h
+++ sort.h
@@ -1,4 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
