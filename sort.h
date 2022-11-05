8420dc9bf52376ae61cf7b40f9d12a5313d80c1c
crysart <idukrystal@gmail.com>
2022-11-05 03:56:10 +0100

    00b

--- sort.h
+++ sort.h
@@ -1,5 +1,10 @@
+#ifndef SORT
+#define SORT
+
 #include <stdio.h>
 
 void print_array(const int *array, size_t size);
 /** void print_list(const listint_t *list); */
 void bubble_sort(int *array, size_t size);
+
+#endif /* SORT */
