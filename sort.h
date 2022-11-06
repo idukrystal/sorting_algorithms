92a771bf61b9d2cbccb2f14ca4ad3bfc79c60e93
crysart <idukrystal@gmail.com>
2022-11-06 14:44:17 +0100

    02

--- sort.h
+++ sort.h
@@ -26,4 +26,5 @@ void bubble_sort(int *array, size_t size);
 void swap(listint_t *tmp, listint_t *tmp2, listint_t **head);
 void insertion_sort_list(listint_t **list);
 
+void selection_sort(int *array, size_t size);
 #endif /* SORT */
