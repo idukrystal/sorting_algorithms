2b67e76c31f11c636cd995cc061bc8428c6407a9
crysart <idukrystal@gmail.com>
2022-11-06 15:23:23 +0100

    59

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,7 +11,7 @@ void selection_sort(int *array, size_t size)
 
 		for (j = i; j < size; j++)
 		{
-			small = (array[j] < array[small]) ? j : small;
+			/*small = (array[j] < array[small]) ? j : small;*/
 		}
 		tmp = array[i];
 		array[i] = array[small];
