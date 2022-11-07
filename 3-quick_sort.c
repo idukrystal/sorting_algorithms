3eea87d41465be32a66fecd0e2a2c56c49cd33f8
crysart <idukrystal@gmail.com>
2022-11-07 03:27:01 +0100

    03 b

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
 
+/**
+ * quick_sort - sorts an array quick
+ * @array: the array
+ * @size: array size
+ */
 void quick_sort(int *array, size_t size)
 {
 	if (array == NULL)
@@ -7,6 +12,13 @@ void quick_sort(int *array, size_t size)
 	quick(array, size, 0, size - 1);
 }
 
+/**
+ * quick - helper function handles recursion
+ * @arr: an array
+ * @size: arr's size
+ * @a: were sort starts
+ * @b: were sort ends
+ */
 void quick(int *arr, size_t size, size_t a, size_t b)
 {
 	size_t i = a - 1;
@@ -31,9 +43,17 @@ void quick(int *arr, size_t size, size_t a, size_t b)
 	quick(arr, size, (i + 1), b);
 }
 
+/**
+ * swapa - swaps elements of an array
+ * @arr: the array
+ * @size: arrays size
+ * @a: element 1
+ * @b: element 2
+ */
 void swapa(int *arr, size_t size, int *a, int *b)
 {
 	int tmp;
+
 	if (*a != *b)
 	{
 		tmp = *a;
